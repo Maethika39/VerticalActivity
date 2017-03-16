0bc81ec9da234fc97a142ac0bcfb6056533e2c80
Win_10 <bluetaiyaki9339@gmail.com>
2017-03-16 11:06:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/win_10/verticalactivity/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.win_10.verticalactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
